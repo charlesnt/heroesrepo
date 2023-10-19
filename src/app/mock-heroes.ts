c6d994df5a1bb6f0cb5fb6251fb5d3f0f695791f
charles ntungane <ntunganecharles@gmail.com>
2023-10-19 11:42:56 -0500

    push changes

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,14 +1,14 @@
 import {Hero} from './hero';
 
 export const HEROES: Hero[]=[
-    { id: 11, name: 'Dr Nice' },
-    { id: 12, name: 'Narco' },
-    { id: 13, name: 'Bombasto' },
-    { id: 14, name: 'Celeritas' },
-    { id: 15, name: 'Magneta' },
-    { id: 16, name: 'RubberMan' },
-    { id: 17, name: 'Dynama' },
-    { id: 18, name: 'Dr IQ' },
-    { id: 19, name: 'Magma' },
-    { id: 20, name: 'Tornado' }
+    { id: 11, name: 'Charles' },
+    { id: 12, name: 'Ntungane' },
+    { id: 13, name: 'Zoey' },
+    { id: 14, name: 'Mahirwe' },
+    { id: 15, name: 'Feza' },
+    { id: 16, name: 'Joyeuse' },
+    { id: 17, name: 'Blessed' },
+    { id: 18, name: 'Family' },
+    { id: 19, name: 'TO be' },
+    { id: 20, name: 'IN jesus name' }
 ];
